4da444c958ec6bb057663b6535db50141bd42155
pingu2017 <pingu2017@naver.com>
2023-12-03 14:34:17 +0900

    [Add] TEST클래스 생성

---
+++ exercise/src/test/java/com/pingu/exercise/api/CityFilterDtoTest.java
@@ -0,0 +1,55 @@
+package com.pingu.exercise.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CityFilterDtoTest {
+
+	@Test
+	void getCityName() {
+		final CityFilterDto cityFilterDto=new CityFilterDto("서울","대한민국", "한국어");
+		final String cityName=cityFilterDto.getCityName();
+		assertEquals("서울", cityName);
+	}
+
+	@Test
+	void getCountryName() {
+	}
+
+	@Test
+	void getRegion() {
+	}
+
+	@Test
+	void setCityName() {
+	}
+
+	@Test
+	void setCountryName() {
+	}
+
+	@Test
+	void setRegion() {
+	}
+
+	@Test
+	void testEquals() {
+	}
+
+	@Test
+	void canEqual() {
+	}
+
+	@Test
+	void testHashCode() {
+	}
+
+	@Test
+	void testToString() {
+	}
+
+	@Test
+	void builder() {
+	}
+}
---
+++ exercise/src/test/java/com/pingu/exercise/service/CityServiceTest.java
@@ -0,0 +1,17 @@
+package com.pingu.exercise.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CityServiceTest {
+
+	@Test
+	void filter() {
+	}
+
+	@Test
+	void getCity() {
+
+	}
+}
